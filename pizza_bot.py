61ce1394ada8981c18707c5bff0d8fceeabe9098
17086 <17086@my.sanctamaria.school.nz>
2022-02-17 13:35:59 +1300

    Added Welcome function
    Added welcome and main function from component 1 version 2

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,6 +1,25 @@
 #Pizza bot program
+import random
+from random import randint
+
+#list of random names
+names = ["Michael", "Jess", "Lynux", "Mikaela", "Trevor", "Sarah", "Jake", "Caitlyn", "Carlos", "Elizabeth"]
 
 # Welcome message with random name
+def welcome():
+    """
+Purpose: To generate a random name from the list and print out a welcome message 
+Parameters: None 
+Returns: None
+
+    """
+    
+    
+    num = randint(0,9)
+    name = (names[num])
+    print("** Welcome to Dream Pizza **")
+    print("** My name is",name,"**")
+    print("** I will be here to help you order your delicious Dream Pizza **")
 
 
 
@@ -67,3 +86,14 @@
 
 
 # Main function 
+def main():
+    welcome()
+    """
+    Purpose:To run all functions
+    Parameters: None 
+    Returns: None
+
+    """
+
+
+main()
