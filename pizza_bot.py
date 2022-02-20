2a6c7f3d847f29682ce55ead2a7fcbf92ecaf55f
17086 <17086@my.sanctamaria.school.nz>
2022-02-21 10:19:35 +1300

    integrated pickup menu to main app
    made pickup or delivery menu into a function and integrated it into main routine

--- pizza_bot.py
+++ pizza_bot.py
@@ -26,7 +26,37 @@ Returns: None
 
 
 
+
+
+
 # Menu for pickup or delivery 
+def pickup():
+    print ("Is your order for pickup or delivery?")
+
+    print (" For pickup enter 1.")
+    print (" For delivery enter 2.")
+
+
+    low = 1
+    high = 2
+
+
+    while True:
+        try:
+            delivery = int(input ("Please enter a number "))
+            if delivery >= 1 and delivery <= 2:
+                if delivery == 1:
+                    print ("Pickup")
+                    break
+
+                elif  delivery == 2:
+                    print ("Delivery")
+                    break
+            else: 
+                print ("Number must be 1 or 2")
+        except ValueError:
+            print ("That is not a valid number.")
+            print ("Please enter 1 or 2.")
 
 
 
@@ -88,6 +118,7 @@ Returns: None
 # Main function 
 def main():
     welcome()
+    pickup()
     """
     Purpose:To run all functions
     Parameters: None 
