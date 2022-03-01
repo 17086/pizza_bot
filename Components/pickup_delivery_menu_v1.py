a99cfea1a895855788cb7e002ac048ecccdcc705
17086 <17086@my.sanctamaria.school.nz>
2022-03-02 09:32:53 +1300

    added version 1 pizza menu
    Created a basic pizza menu

---
+++ Components/pickup_delivery_menu_v1.py
@@ -0,0 +1,19 @@
+# Bugs
+# Will only work for valid input "d" and "p"
+
+# menu so that choose either pickup or delviery 
+
+print ("Do you want your order delivered or are you picking it up?")
+
+print (" For delivery enter d or enter p for pickup.")
+
+delivery = input ()
+if delivery == "d":
+    print ("Delviery")
+
+
+elif  delivery == "p":
+    print ("Pickup")
+
+
+
